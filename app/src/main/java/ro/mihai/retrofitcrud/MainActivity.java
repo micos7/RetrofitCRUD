cfc0e60f1bbfeb138f8702ced4219a21f5a6142f
micos7 <micos7@gmail.com>
2018-08-01 08:06:30 +0300

    Initial commit

---
+++ app/src/main/java/ro/mihai/retrofitcrud/MainActivity.java
@@ -0,0 +1,15 @@
+package ro.mihai.retrofitcrud;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText editTextMail, editTextPassword, editTextName, editTextSchool;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
