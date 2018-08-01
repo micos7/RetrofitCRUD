053dc4d0a61dbd36d4cb3268a89902eeac4eb2ab
micos7 <micos7@gmail.com>
2018-08-01 08:55:26 +0300

    Initialization and validation

--- app/src/main/java/ro/mihai/retrofitcrud/MainActivity.java
+++ app/src/main/java/ro/mihai/retrofitcrud/MainActivity.java
@@ -2,14 +2,78 @@ package ro.mihai.retrofitcrud;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private EditText editTextMail, editTextPassword, editTextName, editTextSchool;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextMail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        editTextName = findViewById(R.id.editTextName);
+        editTextSchool = findViewById(R.id.editTextSchool);
+
+        findViewById(R.id.buttonSignUp).setOnClickListener(this);
+        findViewById(R.id.textViewLogin).setOnClickListener(this);
+    }
+
+    private void userSignup(){
+        String email = editTextMail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        String name = editTextName.getText().toString().trim();
+        String school = editTextSchool.getText().toString().trim();
+
+    // Validation
+        if(email.isEmpty()){
+            editTextMail.setError("Email is required!");
+            editTextMail.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextMail.setError("Enter a valid email!");
+            editTextMail.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()){
+            editTextPassword.setError("Password is required!");
+            editTextPassword.requestFocus();
+            return;
+        }
+        if(password.length()< 6){
+            editTextPassword.setError("Password must be at least 6 charaters");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if(name.isEmpty()){
+            editTextName.setError("Name is required!");
+            editTextName.requestFocus();
+            return;
+        }
+
+        if(school.isEmpty()){
+            editTextSchool.setError("School is required!");
+            editTextSchool.requestFocus();
+            return;
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.buttonSignUp:
+                userSignup();
+                break;
+            case R.id.textViewLogin:
+                break;
+        }
     }
 }
