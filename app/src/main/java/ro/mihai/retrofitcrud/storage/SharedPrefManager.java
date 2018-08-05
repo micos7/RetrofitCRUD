950c9945f0a0d3a9573ccf8882998c9c63f80a3c
micos7 <micos7@gmail.com>
2018-08-05 09:57:45 +0300

    Shared Preferences class

---
+++ app/src/main/java/ro/mihai/retrofitcrud/storage/SharedPrefManager.java
@@ -0,0 +1,63 @@
+package ro.mihai.retrofitcrud.storage;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import ro.mihai.retrofitcrud.models.User;
+
+public class SharedPrefManager {
+
+    private static final String SHARED_PREF_NAME = "my_pref";
+
+    private static SharedPrefManager mInstance;
+    private Context mCtx;
+
+    private SharedPrefManager(Context mCtx){
+        this.mCtx = mCtx;
+    }
+
+    public static synchronized SharedPrefManager getInstance(Context mCtx){
+        if(mInstance == null){
+            mInstance = new SharedPrefManager(mCtx);
+        }
+        return mInstance;
+    }
+
+    public void saveUser(User user){
+        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putInt("id",user.getId());
+        editor.putString("email", user.getEmail());
+        editor.putString("name", user.getName());
+        editor.putString("school", user.getSchool());
+
+        editor.apply();
+    }
+
+    public boolean isLoggedIn(){
+        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+
+        return sharedPreferences.getInt("id", -1) != -1;
+    }
+
+    public User getUser(){
+        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        User user = new User(
+                sharedPreferences.getInt("id",-1),
+            sharedPreferences.getString("email", null),
+            sharedPreferences.getString("name", null),
+            sharedPreferences.getString("school", null)
+        );
+
+        return  user;
+    }
+
+    public void clear(){
+        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.apply();
+    }
+
+}
