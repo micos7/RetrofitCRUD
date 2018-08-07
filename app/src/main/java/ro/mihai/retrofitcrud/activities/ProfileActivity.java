93ec154c81da922c5e77bad269e78b44cf7ca57b
micos7 <micos7@gmail.com>
2018-08-07 20:23:18 +0300

    Fragments

--- app/src/main/java/ro/mihai/retrofitcrud/activities/ProfileActivity.java
+++ app/src/main/java/ro/mihai/retrofitcrud/activities/ProfileActivity.java
@@ -1,15 +1,23 @@
 package ro.mihai.retrofitcrud.activities;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import ro.mihai.retrofitcrud.R;
+import ro.mihai.retrofitcrud.fragments.HomeFragment;
+import ro.mihai.retrofitcrud.fragments.SettingsFragment;
+import ro.mihai.retrofitcrud.fragments.UsersFragment;
 import ro.mihai.retrofitcrud.models.User;
 import ro.mihai.retrofitcrud.storage.SharedPrefManager;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
 
 
@@ -18,8 +26,16 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        
+        BottomNavigationView navigationView = findViewById(R.id.bottom_nav);
+        navigationView.setOnNavigationItemSelectedListener(this);
+        displayFragment(new HomeFragment());
+    }
 
+    private void displayFragment(Fragment fragment){
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.relativeLayout, fragment)
+                .commit();
     }
 
     @Override
@@ -31,5 +47,26 @@ public class ProfileActivity extends AppCompatActivity {
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(intent);
         }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        switch(item.getItemId()){
+            case R.id.menu_home:
+                fragment = new HomeFragment();
+                break;
+            case R.id.menu_users:
+                fragment = new UsersFragment();
+                break;
+            case R.id.menu_settings:
+                fragment = new SettingsFragment();
+                break;
+        }
+        if(fragment != null){
+            displayFragment(fragment);
+        }
+        return false;
     }
 }
