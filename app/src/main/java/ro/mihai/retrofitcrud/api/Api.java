443f3e808551c20e0cba9937a5f8609f69e769db
micos7 <micos7@gmail.com>
2018-08-08 08:25:27 +0300

    Update endpoint

--- app/src/main/java/ro/mihai/retrofitcrud/api/Api.java
+++ app/src/main/java/ro/mihai/retrofitcrud/api/Api.java
@@ -3,9 +3,13 @@ package ro.mihai.retrofitcrud.api;
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 import ro.mihai.retrofitcrud.models.DefaultResponse;
 import ro.mihai.retrofitcrud.models.LoginResponse;
+import ro.mihai.retrofitcrud.models.UsersResponse;
 
 public interface Api {
     @FormUrlEncoded
@@ -24,4 +28,15 @@ public interface Api {
             @Field("password") String password
     );
 
+    @GET("allusers")
+    Call<UsersResponse> getUsers();
+
+    @FormUrlEncoded
+    @PUT("updateuser/{id}")
+    Call<LoginResponse> updateUser(
+            @Path("id") int id,
+            @Field("email") String email,
+            @Field("name") String name,
+            @Field("school") String school
+    );
 }
