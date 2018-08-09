7a7931843a8b050ef0fbdbd3b2e7c8557f842536
micos7 <micos7@gmail.com>
2018-08-09 20:34:03 +0300

    Response

---
+++ app/src/main/java/ro/mihai/retrofitcrud/models/UsersResponse.java
@@ -0,0 +1,23 @@
+package ro.mihai.retrofitcrud.models;
+
+import java.util.List;
+
+public class UsersResponse {
+    private boolean error;
+    private List<User> users;
+
+
+    public UsersResponse(boolean error, List<User> users) {
+        this.error = error;
+        this.users = users;
+    }
+
+    public boolean isError() {
+        return error;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+}
