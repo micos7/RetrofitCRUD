6563fa9c1221d631a52a20b384b15bb828c64b07
micos7 <micos7@gmail.com>
2018-08-09 20:33:12 +0300

    Fragments class

--- app/src/main/java/ro/mihai/retrofitcrud/fragments/SettingsFragment.java
+++ app/src/main/java/ro/mihai/retrofitcrud/fragments/SettingsFragment.java
@@ -4,16 +4,115 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import retrofit2.Callback;
+import retrofit2.Response;
 import ro.mihai.retrofitcrud.R;
+import retrofit2.Call;
+import ro.mihai.retrofitcrud.api.RetrofitClient;
+import ro.mihai.retrofitcrud.models.LoginResponse;
+import ro.mihai.retrofitcrud.models.User;
+import ro.mihai.retrofitcrud.storage.SharedPrefManager;
 
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends Fragment implements View.OnClickListener {
+    private EditText editTextName, editTextEmail, editTextSchool;
+    private EditText editTextcurrentPassword, editTextNewPassword;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.settings_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        editTextEmail = view.findViewById(R.id.editTextEmail);
+        editTextName = view.findViewById(R.id.editTextName);
+        editTextSchool = view.findViewById(R.id.editTextSchool);
+        editTextcurrentPassword = view.findViewById(R.id.editTextCurrentPassword);
+        editTextNewPassword = view.findViewById(R.id.editTextNewPassword);
+
+        view.findViewById(R.id.buttonSave).setOnClickListener(this);
+        view.findViewById(R.id.buttonChangePassword).setOnClickListener(this);
+        view.findViewById(R.id.buttonLogout).setOnClickListener(this);
+        view.findViewById(R.id.buttonDelete).setOnClickListener(this);
+    }
+
+    private void updateProfile() {
+        String email = editTextEmail.getText().toString().trim();
+        String name = editTextName.getText().toString().trim();
+        String school = editTextSchool.getText().toString().trim();
+
+        // Validation
+        if (email.isEmpty()) {
+            editTextEmail.setError("Email is required!");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError("Enter a valid email!");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+
+        if (name.isEmpty()) {
+            editTextName.setError("Name is required!");
+            editTextName.requestFocus();
+            return;
+        }
+
+        if (school.isEmpty()) {
+            editTextSchool.setError("School is required!");
+            editTextSchool.requestFocus();
+            return;
+        }
+
+        User user = SharedPrefManager.getInstance(getActivity()).getUser();
+
+        Call<LoginResponse> call = RetrofitClient.getInstance()
+                .getApi().updateUser(
+                        user.getId(),email,name,school
+                );
+
+        call.enqueue(new Callback<LoginResponse>() {
+            @Override
+            public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
+                Toast.makeText(getActivity(), response.body().getMessage(), Toast.LENGTH_SHORT).show();
+
+                if(!response.body().isError()){
+                    SharedPrefManager.getInstance(getActivity()).saveUser(response.body().getUser());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<LoginResponse> call, Throwable t) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.buttonSave:
+                updateProfile();
+                break;
+            case R.id.buttonChangePassword:
+                break;
+            case R.id.buttonLogout:
+                break;
+            case R.id.buttonDelete:
+                break;
+        }
     }
 }
