8817ff70f958fd91ed5de292b697b70ae11590b4
micos7 <micos7@gmail.com>
2018-08-12 09:05:54 +0300

    Basic auth

--- app/src/main/java/ro/mihai/retrofitcrud/api/RetrofitClient.java
+++ app/src/main/java/ro/mihai/retrofitcrud/api/RetrofitClient.java
@@ -1,17 +1,42 @@
 package ro.mihai.retrofitcrud.api;
 
+import android.util.Base64;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-
+    private final static String AUTH = "Basic "+ Base64.encodeToString(("user:test").getBytes(), Base64.NO_WRAP);
     private final static String BASE_URL = "http://192.168.0.101/Api/public/";
     private static RetrofitClient mInstance;
     private Retrofit retrofit;
 
     private RetrofitClient(){
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .addInterceptor(
+                        new Interceptor() {
+                            @Override
+                            public Response intercept(Chain chain) throws IOException {
+                                Request original = chain.request();
+
+                                Request.Builder requestBuilder = original.newBuilder()
+                                        .addHeader("Authorization",AUTH )
+                                        .method(original.method(),original.body());
+                                Request request = requestBuilder.build();
+                                return  chain.proceed(request);
+                            }
+                        }
+                ).build();
+
         retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
                 .build();
     }
 
